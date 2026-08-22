a894890a3529669243c9768cd181f7b71c82ba08
Sercan Birsen <1993sercanbirsen@gmail.com>
2026-08-22 17:15:38 +0200

    Add country payment analysis query

--- 01-Vendor-Payment-Analysis/queries.sql
+++ 01-Vendor-Payment-Analysis/queries.sql
@@ -1,1 +1,21 @@
--- SQL queries for Vendor Payment Analysis
+/*=========================================================
+Project: Vendor Payment Analysis
+Database: SQLite
+Author: Sercan Birsen
+=========================================================*/
+
+
+/*=========================================================
+Query 1
+Business Question:
+Which countries have received the highest total payments?
+=========================================================*/
+
+SELECT
+    v.country,
+    SUM(p.payment_amount_num) AS Total_Payment
+FROM Vendors v
+JOIN Payments p
+ON v.vendor_id_num = p.vendor_id_num
+GROUP BY v.country
+ORDER BY Total_Payment DESC;
